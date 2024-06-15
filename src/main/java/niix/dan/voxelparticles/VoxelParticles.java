fcacda8d2ab97716bba46273ba0aae213f94fc12
Niix <46878004+Niix-Dan@users.noreply.github.com>
2024-06-15 08:27:31 -0300

    Otimizacao basica

--- src/main/java/niix/dan/voxelparticles/VoxelParticles.java
+++ src/main/java/niix/dan/voxelparticles/VoxelParticles.java
@@ -3,6 +3,7 @@ package niix.dan.voxelparticles;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -12,7 +13,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class VoxelParticles extends JavaPlugin {
 
@@ -21,6 +24,8 @@ public final class VoxelParticles extends JavaPlugin {
 
     private double scaleFactor = 1;
     private int particleDensity = 1;
+    private int durationTicks = 20 * 60;
+    private double compressionDistance = 0.2;
 
     @Override
     public void onEnable() {
@@ -28,6 +33,15 @@ public final class VoxelParticles extends JavaPlugin {
         loadModel("plugins/VoxelParticles/model.obj");
 
         getCommand("showmodel").setExecutor(this);
+
+
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+
+        scaleFactor = getConfig().getDouble("scaleFactor", 1);
+        particleDensity = getConfig().getInt("particleDensity", 1);
+        durationTicks = getConfig().getInt("durationTicks", 20 * 60);
+        compressionDistance = getConfig().getDouble("compressionDistance", 0.2);
     }
 
     @Override
@@ -75,46 +89,39 @@ public final class VoxelParticles extends JavaPlugin {
     }
 
     private void showModel(Location origin) {
-        int durationTicks = 20 * 60;
+        List<Location> allParticles = new ArrayList<>();
+
+        for (Face face : modelFaces) {
+            int[] vertexIndices = face.getVertexIndices();
+            Location[] vertices = new Location[vertexIndices.length];
+            for (int i = 0; i < vertexIndices.length; i++) {
+                vertices[i] = modelVertices.get(vertexIndices[i]).clone();
+                vertices[i].setWorld(origin.getWorld());
+                vertices[i].add(origin);
+            }
+
+            for (int i = 0; i < vertices.length; i++) {
+                Location start = vertices[i];
+                Location end = vertices[(i + 1) % vertices.length];
+                allParticles.addAll(generateLineParticles(start, end));
+            }
+        }
+
+        List<Location> compressedParticles = compressParticles(allParticles);
 
         int task = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            for (Face face : modelFaces) {
-                int[] vertexIndices = face.getVertexIndices();
-                Location[] vertices = new Location[vertexIndices.length];
-                for (int i = 0; i < vertexIndices.length; i++) {
-                    vertices[i] = modelVertices.get(vertexIndices[i]).clone();
-                    vertices[i].setWorld(origin.getWorld());
-                    vertices[i].add(origin);
-                }
-                Location center = getFaceCenter(vertices);
-                for (int i = 0; i < vertices.length; i++) {
-                    Location start = vertices[i];
-                    Location end = vertices[(i + 1) % vertices.length];
-                    drawLineParticles(origin, start, end, center);
-                }
+            for (Location particleLocation : compressedParticles) {
+                Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+                    origin.getWorld().spawnParticle(Particle.FLAME, particleLocation, 0, 0, 0, 0, 0);
+                });
             }
         }, 0L, 5L);
 
         Bukkit.getScheduler().runTaskLater(this, () -> Bukkit.getScheduler().cancelTask(task), durationTicks);
     }
 
-    private Location getFaceCenter(Location[] vertices) {
-        double centerX = 0;
-        double centerY = 0;
-        double centerZ = 0;
-        for (Location vertex : vertices) {
-            centerX += vertex.getX();
-            centerY += vertex.getY();
-            centerZ += vertex.getZ();
-        }
-        int numVertices = vertices.length;
-        centerX /= numVertices;
-        centerY /= numVertices;
-        centerZ /= numVertices;
-        return new Location(vertices[0].getWorld(), centerX, centerY, centerZ);
-    }
-
-    private void drawLineParticles(Location origin, Location start, Location end, Location center) {
+    private List<Location> generateLineParticles(Location start, Location end) {
+        List<Location> particleLocations = new ArrayList<>();
         double distance = start.distance(end);
         int particleCount = (int) Math.ceil(distance * particleDensity);
 
@@ -122,33 +129,59 @@ public final class VoxelParticles extends JavaPlugin {
         double deltaY = (end.getY() - start.getY()) / particleCount;
         double deltaZ = (end.getZ() - start.getZ()) / particleCount;
 
-        List<Location> particleLocations = new ArrayList<>();
         for (int i = 0; i < particleCount; i++) {
             Location particleLocation = start.clone().add(deltaX * i, deltaY * i, deltaZ * i);
             particleLocations.add(particleLocation);
         }
-
-        List<Location> compressedParticles = compressParticles(particleLocations);
-        
-        for (Location particleLocation : compressedParticles) {
-            Bukkit.getScheduler().runTask(this, () -> {
-                origin.getWorld().spawnParticle(Particle.FLAME, particleLocation, 0, 0, 0, 0, 0);
-            });
-        }
+        return particleLocations;
     }
 
     private List<Location> compressParticles(List<Location> particleLocations) {
-        double compressionDistance = 0.2; // Distância mínima para agrupar partículas
         List<Location> compressedParticles = new ArrayList<>();
-        Location lastLocation = null;
+        Set<Location> visited = new HashSet<>();
 
         for (Location loc : particleLocations) {
-            if (lastLocation == null || loc.distance(lastLocation) > compressionDistance) {
+            if (visited.contains(loc)) {
+                continue;
+            }
+
+            List<Location> nearParticles = getNearParticles(loc, particleLocations, compressionDistance);
+            visited.addAll(nearParticles);
+
+            if (nearParticles.size() > 1) {
+                Location averageLocation = averageLocation(nearParticles);
+                compressedParticles.add(averageLocation);
+            } else {
                 compressedParticles.add(loc);
-                lastLocation = loc;
             }
         }
         return compressedParticles;
+    }
+
+    private List<Location> getNearParticles(Location loc, List<Location> particleLocations, double compressionDistance) {
+        List<Location> nearParticles = new ArrayList<>();
+        for (Location other : particleLocations) {
+            if (loc.distance(other) <= compressionDistance) {
+                nearParticles.add(other);
+            }
+        }
+        return nearParticles;
+    }
+
+    private Location averageLocation(List<Location> locations) {
+        double sumX = 0;
+        double sumY = 0;
+        double sumZ = 0;
+        World world = locations.get(0).getWorld();
+
+        for (Location loc : locations) {
+            sumX += loc.getX();
+            sumY += loc.getY();
+            sumZ += loc.getZ();
+        }
+
+        int count = locations.size();
+        return new Location(world, sumX / count, sumY / count, sumZ / count);
     }
 
     private static class Face {
